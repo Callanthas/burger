3d3ef36c0c3f49b2fc5c42e6a797ee2547091664
Callanthas <lord_of_light1@hotmail.com>
2019-01-26 01:10:39 -0600

    set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mexican Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('French Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Japanese Burger', FALSE);
